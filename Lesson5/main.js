6cafc86a41a36c3b8b7db4b18206eae967ec5fe1
scrubbychris <45634931+scrubbychris@users.noreply.github.com>
2019-09-11 10:42:21 -0400

    Add files via upload

---
+++ Lesson5/main.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'greensock.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'red',
+        variantImage: 'redsock.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    },
+    removeFromCart() {
+      this.cart -= 1
+    }
+  }
+})
